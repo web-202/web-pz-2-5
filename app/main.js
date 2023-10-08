586cd4c8bced21b69d7cad76e19f623b8c688c26
Pavlo740 <pavlo.dykyi@viti.edu.ua>
2023-10-08 23:51:30 +0300

    Added Save and Paste buttons

--- app/main.js
+++ app/main.js
@@ -1,103 +1,118 @@
-let a = '';
-let b = '';
-let sign = '';
+let a = "";
+let b = "";
+let sign = "";
 let finish = false;
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', '×', '÷', '%', '+/-'];
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["-", "+", "×", "÷", "%", "+/-"];
 
-const out = document.querySelector('.calculator-screen p');
+const out = document.querySelector(".calc-screen p");
 
+// Function to save result to LocalStorage
+function saveResult() {
+  if (a !== "") {
+    localStorage.setItem("calculatorResult", a);
+    console.log("Result saved to LocalStorage:", a);
+  }
+}
 
+// Function to paste result from LocalStorage
+function pasteResult() {
+  const savedResult = localStorage.getItem("calculatorResult");
+  if (savedResult !== null) {
+    a = savedResult;
+    out.textContent = a; // Update the calculator screen
+    console.log("Result pasted from LocalStorage:", a);
+  }
+}
 
+// Event listeners for Save and Paste buttons
+document.querySelector(".save").onclick = saveResult;
+document.querySelector(".paste").onclick = pasteResult;
+
+//Clear all
 function clearAll() {
-    a = '';
-    b = '';
-    sign = '';
-    finish = false;
-    out.textContent = 0;
+  a = "";
+  b = "";
+  sign = "";
+  finish = false;
+  out.textContent = 0;
 }
 
-document.querySelector('.ac').onclick = clearAll;
+//Add Clear all
+document.querySelector(".ac").onclick = clearAll;
 
+//Use button
+document.querySelector(".buttons").onclick = (event) => {
+  //Click a not button
+  if (!event.target.classList.contains("btn")) return;
+  //Click a button clearAll
+  if (event.target.classList.contains("ac")) return;
 
-document.querySelector('.buttons').onclick = (event) => {
+  out.textContent = "";
+  //Get clicked button
+  const key = event.target.textContent;
 
-    if (!event.target.classList.contains('btn')) return;
+  //If clicked 0-9 or .
+  if (digit.includes(key)) {
+    if (b === "" && sign === "") {
+      a += key;
+      out.textContent = a;
+    } else if (a !== "" && b !== "" && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    } else {
+      b += key;
+      out.textContent = b;
+      return;
+    }
+  }
 
-    if (event.target.classList.contains('ac')) return;
+  //If clicked + - * / or % or +/-
+  if (action.includes(key)) {
+    if (key === "%") {
+      a = a / 100;
+      out.textContent = a;
+      return;
+    } else if (key === "+/-") {
+      a = -a;
+      out.textContent = a;
+      return;
+    } else {
+      sign = key;
+      out.textContent = sign;
+      return;
+    }
+  }
 
-    out.textContent = '';
-
-    const key = event.target.textContent;
-
-
-    if (digit.includes(key)) {
-        if (b === '' && sign === '') {
-            a += key;
-            out.textContent = a;
+  //Clicked =
+  if (key === "=") {
+    if (b === "") b = a;
+    switch (sign) {
+      case "+":
+        a = +a + +b;
+        break;
+      case "-":
+        a = a - b;
+        break;
+      case "×":
+        a = a * b;
+        break;
+      case "÷":
+        if (b === "0") {
+          out.textContent = "Error";
+          a = "";
+          b = "";
+          sign = "";
+          return;
         }
-        else if (a !== '' && b !== '' && finish) {
-            b = key;
-            finish = false;
-            out.textContent = b;
-        }
-
-        else {
-            b += key;
-            out.textContent = b;
-            return
-        }
+        a = a / b;
+        break;
     }
 
-
-    if (action.includes(key)) {
-        if (key === '%') {
-            a = a / 100;
-            out.textContent = a;
-            return;
-        }
-        else if (key === '+/-') {
-            a = -a;
-            out.textContent = a;
-            return;
-        }
-        else {
-            sign = key;
-            out.textContent = sign;
-            return;
-        }
-    }
-
-
-    if (key === '=') {
-        if (b === '') b = a;
-        switch (sign) {
-            case '+':
-                a = (+a) + (+b);
-                break;
-            case '-':
-                a = a - b;
-                break;
-            case '×':
-                a = a * b;
-                break;
-            case '÷':
-                if (b === '0') {
-                    out.textContent = 'Error'
-                    a = '';
-                    b = '';
-                    sign = '';
-                    return
-                }
-                a = a / b;
-                break;
-        }
-
-        finish = true
-        out.textContent = a;
-        console.log(a + '' + sign + '' + b);
-    }
-    
-}
-
+    finish = true;
+    out.textContent = a;
+    console.log(a + "" + sign + "" + b);
+  }
+};
