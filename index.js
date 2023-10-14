3902a2f7ce437ecf468ac887d68fa716cefb86c0
User052 <user052@MacBook-Air-Mac052.local>
2023-10-14 10:34:51 +0300

    v1.1 Radchenko

--- index.js
+++ index.js
@@ -30,3 +30,27 @@ function makeNegative() {
         screen.textContent = currentInput;
     }
 }
+
+const colorButton = document.getElementById('colorButton');
+
+
+colorButton.addEventListener('click', changeInterfaceColor);
+
+
+function changeInterfaceColor() {
+
+    const calculator = document.querySelector('.calculator');
+    calculator.style.backgroundColor = getRandomColor(); 
+
+    const screen = document.getElementById('screen');
+    screen.style.color = getRandomColor();
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
