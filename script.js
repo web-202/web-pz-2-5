d1bb7db406a52efefc68a39bbf459af4df03ffac
Oleksii Shevchenko <lesha30032003@gmail.com>
2023-10-14 13:46:11 +0300

    add-function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let displayValue = '0';
+let currentResult = 0; 
+
 function clearDisplay() {
     displayValue = '0';
     updateDisplay();
@@ -29,3 +31,35 @@ function calculatePercentage(displayValue) {
 function updateDisplay() {
     document.getElementById('display').textContent = displayValue;
 }
+
+document.getElementById('change-color-button').addEventListener('click', () => {
+    const calculator = document.querySelector('.calculator');
+  
+    if (calculator.classList.contains('red')) {
+      calculator.classList.remove('red');
+      calculator.classList.add('green'); 
+      document.querySelectorAll('.btn').forEach((button) => {
+        button.style.backgroundColor = '#ff00ff';
+      });
+    } else {
+      calculator.classList.remove('green'); 
+      calculator.classList.add('red');
+      document.querySelectorAll('.btn').forEach((button) => {
+        button.style.backgroundColor = '#0000ff';
+      });
+    }
+  });
+
+function saveResult() {
+    const display = document.getElementById('display');
+    const displayValue = display.textContent;
+    currentResult = parseFloat(displayValue);
+}
+function pasteResult() {
+    const display = document.getElementById('display');
+    currentInput = currentResult.toString(); 
+    display.textContent = currentInput;
+}
+
+  
+  
