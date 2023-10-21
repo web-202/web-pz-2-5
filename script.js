749c421bcc8a187153f269365ed5f560807bae4a
Oleksii Shevchenko <lesha30032003@gmail.com>
2023-10-21 14:05:12 +0300

    new-fix

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let displayValue = '0';
 let currentResult = 0; 
+let lastOperation = null;
+let percentageResult = null;
 
 function clearDisplay() {
     displayValue = '0';
+    percentageResult = null;
     updateDisplay();
 }
 
@@ -23,10 +26,37 @@ function calculate() {
     }
     updateDisplay();
 }
-function calculatePercentage(displayValue) {
-    const result = displayValue / 100;
-    return `${percent}${result}`;
+function calculatePercentage() {
+  const display = document.getElementById('display');
+  const currentInput = parseFloat(display.textContent); 
+
+  if (!isNaN(currentInput)) {
+    percentageResult = currentInput / 100; 
+    displayValue = percentageResult.toString(); 
   }
+  updateDisplay();
+}
+
+function performOperation(operator) {
+  if (lastOperation !== null) {
+      calculate();
+  }
+
+  if (operator === '%') {
+      calculatePercentage();
+  } else if (percentageResult !== null) {
+      displayValue = percentageResult.toString() + operator;
+      percentageResult = null;
+  } else {
+      displayValue += operator;
+  }
+
+  lastOperation = operator;
+  updateDisplay();
+}
+
+
+
 
 function updateDisplay() {
     document.getElementById('display').textContent = displayValue;
@@ -51,15 +81,17 @@ document.getElementById('change-color-button').addEventListener('click', () => {
   });
 
 function saveResult() {
-    const display = document.getElementById('display');
-    const displayValue = display.textContent;
-    currentResult = parseFloat(displayValue);
+  const display = document.getElementById('display');
+  const displayValue = display.textContent;
+  currentResult = parseFloat(displayValue);
 }
 function pasteResult() {
-    const display = document.getElementById('display');
-    currentInput = currentResult.toString(); 
-    display.textContent = currentInput;
+  const display = document.getElementById('display');
+  currentInput = currentResult.toString(); 
+  display.textContent = currentInput;
 }
+
+
 
   
   
