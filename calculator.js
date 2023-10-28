f597821edfd17cabd73ea9d3a5af6dfe71b03c9a
AnnetShev <shevanna03@gmail.com>
2023-10-28 17:13:56 +0300

    add color, copy,paste

--- calculator.js
+++ calculator.js
@@ -1,80 +1,107 @@
-let a='';
-let b='';
-let sing='';
-let finish=false;
+let a = '';
+let b = '';
+let sing = '';
+let finish = false;
+let clipboard = '';
 
+let colorButtons = document.querySelectorAll(".color-change");
+let colorButton = document.querySelectorAll("button");
+const genNew = document.getElementById("genNew");
 
-const digit =['0','1','2','3','4','5','6','7','8','9','.'];
-const alt =['*','/','-','+','%'];
+const copyButton = document.querySelector('.calculateCopy');
+const pasteButton = document.querySelector('.calculatePaste');
 
-const out= document.querySelector('.win');
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const alt = ['*', '/', '-', '+', '%']; // Додано масив "alt"
 
-function clearAll(){
-  a='';
-  b='';
-  sing='';
-  finish=false;
-  out.textContent=0;
+let currentInput = document.querySelector('.win');
+
+function clearAll() {
+  a = '';
+  b = '';
+  sing = '';
+  finish = false;
+  currentInput.textContent = '0';
 }
 
-document.querySelector('.clearDisplay').onclick =clearAll;
+document.querySelector('.clearDisplay').onclick = clearAll;
 
-document.querySelector('.buttons').onclick = (event) =>{
- if(!event.target.classList.contains('button'))return;
+document.querySelector('.buttons').onclick = (event) => {
+  if (!event.target.classList.contains('button')) return;
 
- out.textContent =' '; 
- const key = event.target.textContent;
+  const key = event.target.textContent;
 
- if(digit.includes(key)){
-    if(b==='' && sing === ''){
-      a+=key;
-      out.textContent =a;
-
+  if (digit.includes(key)) {
+    if (b === '' && sing === '') {
+      a += key;
+      currentInput.textContent = a;
+    } else if (a !== '' && b !== '' && finish) {
+      clearAll();
+      a = key;
+      currentInput.textContent = a;
+    } else {
+      b += key;
+      currentInput.textContent = b;
     }
-    else if( a!=='' && b!=='' && finish){}
-    else{
-      b +=key;
-      out.textContent =a;
-    }
-    console.log(a,b, sing);
     return;
   }
 
-  if(alt.includes(key)){
+  if (alt.includes(key)) {
     sing = key;
-    out.textContent = sing;
-    console.log(a,b,sing);
+    currentInput.textContent = sing;
     return;
   }
 
- if(key === '='){
-  switch(sing){
-    case "+":
-      a = (+a) +(+b);
-      break;
+  if (key === '=') {
+    switch (sing) {
+      case "+":
+        a = (+a) + (+b);
+        break;
       case "-":
-        a = a-b;
-      break;
+        a = a - b;
+        break;
       case "*":
-          a = a*b;
-      break;
+        a = a * b;
+        break;
       case "/":
-        if( b===0){
-          out.textContent='erorr';
-          a='';
-          b='';
-          sing='';
+        if (b === '0') {
+          currentInput.textContent = 'error';
+          a = '';
+          b = '';
+          sing = '';
           return;
         }
-        a = a/b;
-      break;
-      case "%":
-        a= a/100;
+        a = a / b;
         break;
+      case "%":
+        a = a / 100;
+        break;
+    }
+    finish = true;
+    currentInput.textContent = a;
   }
-  finish=true;
-  out.textContent=a;
-  console.log(a,b,sing)
- }
 
+  colorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      colorButton.forEach((button) => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        button.style.backgroundColor = "#" + randomColor;
+        button.style.color = "#fff";
+      });
+    });
+  });
+}
+
+let result = 0;
+
+function saveResult() {
+  result = parseFloat(currentInput.textContent);
+}
+
+function pasteResult() {
+  currentInput.textContent = result;
+  a = result;
+  b = '';
+  sing = '';
+  finish = true;
 }
