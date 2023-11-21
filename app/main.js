b107331abb6959da78e404b9c1d2477f6394177b
BogdanPalukh <palukh.bogdan1337@gmail.com>
2023-11-21 13:14:24 +0300

    fix1.1

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 let screen = document.querySelector('.screen');
 let currentValue = '';
+const colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33A1', '#33A1FF'];
+const colors1 = ['#FF2356', '#444444', '#666666', '#777777', '#234521'];
 function app(value) {
   console.log(value)
   currentValue += value;
@@ -33,4 +35,27 @@ function changeSign() {
     currentValue = (parseFloat(currentValue) * -1).toString();
     screen.textContent = currentValue;
   }
+}
+function changeColor(){
+  const randomColorIndex = Math.floor(Math.random() * colors.length);
+  const randomColor = colors[randomColorIndex];
+  document.querySelector('.calc').style.backgroundColor = randomColor;
+
+  const randomColorIndex1 = Math.floor(Math.random() * colors1.length);
+  const randomColor1 = colors1[randomColorIndex];
+
+  const buttons = document.querySelectorAll('.btn');
+  console.log(buttons)
+  buttons.forEach(button => {
+    button.style.backgroundColor = randomColor1;
+  });
+}
+function save(){
+  const currentInput = document.querySelector('.screen').textContent;
+  localStorage.setItem('savedData', currentInput);
+  console.log(currentInput)
+}
+function paste(){
+  const currentSave = localStorage.getItem('savedData').toString()
+  screen.textContent = currentSave;
 }
