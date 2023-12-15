2f06b698b145df48393292ad8c192060036414e6
Alexeylyhov <Alexeylyhov@icloud.com>
2023-12-15 12:31:57 +0200

    sers

--- app/main.js
+++ app/main.js
@@ -4,6 +4,31 @@ let currentOperator = '';
 let previousInput = '';
 let saveRes = null;
 
+function calculateFibonacci() {
+    // Отримання введеного користувачем числа
+    let inputNumber = document.getElementById("screen").innerText;
+    console.log(inputNumber)
+    // Перевірка на валідність числа
+    if (isNaN(inputNumber) || inputNumber > 0) {
+        // alert("Please enter a non-negative number.");
+        // return;
+    }
+
+    // Обчислення числа Фібоначчі
+    var result = fibonacci(parseInt(inputNumber));
+    console.log(result)
+    // Виведення результату
+    document.getElementById("screen").innerText = result;
+}
+
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
 function appendToScreen(value) {
     currentInput += value;
     screen.textContent = currentInput;
@@ -41,24 +66,23 @@ function makeNegative() {
 document.addEventListener("DOMContentLoaded", function() {
     const changeColorButton = document.getElementById("color_change");
     const colorButtons = document.querySelectorAll("button");
-  
+
     changeColorButton.addEventListener("click", function() {
-      const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-      colorButtons.forEach(function(button) {
-        button.style.backgroundColor = randomColor;
-      });
+        const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        colorButtons.forEach(function(button) {
+            button.style.backgroundColor = randomColor;
+        });
     });
-  });
+});
 let savedResult = null;
 
 function saveResult() {
-  savedResult = currentInput;
+    savedResult = currentInput;
 }
 
 function pasteResult() {
     if (savedResult !== null) {
-      currentInput += savedResult;
-      screen.textContent = currentInput;
+        currentInput += savedResult;
+        screen.textContent = currentInput;
     }
-  }
-  
+}
